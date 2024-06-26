a8c790e9295f820bf88826bf352953bdde2972f8
= <=>
2024-06-26 21:10:34 +0300

    Add project files.

---
+++ MusteriManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    internal class MusteriManager
+    {
+        private Musteri _musteri;
+        public MusteriManager(Musteri musteri)
+        {
+            _musteri = musteri;
+        }
+        public void MusteriEkle()
+        {
+            Console.WriteLine("Müsteri ekleniyor : " + _musteri.FirstName + "|| Id => " + _musteri.Id);
+        }
+        public void MüsteriListele()
+        {
+            Console.WriteLine("Müsteri listeniyor : " + _musteri.FirstName + "|| Id => " + _musteri.Id);
+        }
+        public void MüsteriSil()
+        {
+            Console.WriteLine("Müsteri siliniyor : " + _musteri.FirstName + "|| Id => " + _musteri.Id);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri = new Musteri();
+            musteri.FirstName = "Ali";
+            musteri.LastName = "Demiroğ";
+            musteri.DateOfBirth = DateTime.Now;
+            musteri.Id = 1;
+            musteri.Email = "dqjeqejwqjewıjqew@gmail.com";
+
+
+            Musteri musteri2 = new Musteri();
+            musteri2.FirstName = "Erkan";
+            musteri2.LastName = "Sayılır";
+            musteri2.DateOfBirth = DateTime.Now;
+            musteri2.Id = 2;
+            musteri2.Email = "kelimess@gmail.com";
+
+
+            MusteriManager musteriManager = new MusteriManager(musteri);
+            musteriManager.MusteriEkle();
+            musteriManager.MüsteriListele();
+            musteriManager.MüsteriSil();
+
+            Console.WriteLine("---------------------------");
+            Console.WriteLine("---------------------------");
+
+            MusteriManager musteriManager2 = new MusteriManager(musteri2);
+            musteriManager2.MusteriEkle();
+            musteriManager2.MüsteriListele();
+            musteriManager2.MüsteriSil();
+
+            Console.ReadLine();
+        }
+    }
+}
